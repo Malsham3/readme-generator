cde3a4b85684cd347c5edd14f8ed84e4cd64503f
Mostafa <m.alsham627@gmail.com>
2020-11-27 19:10:47 -0700

    started generateMarkdownf fn

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,21 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(
+  {
+  title,
+  description,
+  installation,
+  usage,
+  contribution,
+  test,
+  license,
+  github,
+  email
 }
+){
+  return `# ${title}`;
+
+}
+
+
 
 module.exports = generateMarkdown;
